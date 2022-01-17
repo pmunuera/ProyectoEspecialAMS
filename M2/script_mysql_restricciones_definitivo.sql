9d1af7f7a477eef3b34f38a209f019a436519c85
Rafael-Puerta <rafapc1992@gmail.com>
2022-01-17 10:38:11 +0100

    Modificación del script para una lectura más sencilla

--- M2/script_mysql_restricciones_definitivo.sql
+++ M2/script_mysql_restricciones_definitivo.sql
@@ -1,46 +1,54 @@
 use PROYECTO;
 ALTER DATABASE PROYECTO CHARACTER SET utf8 COLLATE utf8_general_ci;
-alter table USER modify id_user int unsigned auto_increment primary key;
-alter table USER modify username varchar(15) not null;
-alter table USER modify password varchar(40) not null;
-alter table USER add constraint unique_user unique (username);
-alter table USER modify date_creation timestamp default localtimestamp();
 
-alter table PROYECTO_CHARACTER modify id_character int UNSIGNED AUTO_INCREMENT PRIMARY KEY;
-alter table PROYECTO_CHARACTER modify name varchar(40) not null;
-alter table PROYECTO_CHARACTER add constraint unique_name unique (name);
-alter table PROYECTO_CHARACTER modify date_creation timestamp default localtimestamp();
+alter table USER 
+	modify id_user int unsigned auto_increment primary key,
+	modify username varchar(15) not null,
+	modify password varchar(40) not null,
+	add constraint unique_user unique (username),
+	modify date_creation timestamp default localtimestamp();
 
-alter table ADVENTURE modify id_adventure int unsigned auto_increment primary key;
-alter table ADVENTURE modify name varchar(40) not null unique;
-alter table ADVENTURE modify description varchar(100) not null;
-alter table ADVENTURE modify date_creation timestamp default localtimestamp();
+alter table PROYECTO_CHARACTER 
+	modify id_character int UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	modify name varchar(40) not null,
+	add constraint unique_name unique (name),
+	modify date_creation timestamp default localtimestamp();
 
-alter table STEP modify id_step int unsigned auto_increment primary key;
-alter table STEP modify description varchar(100) not null;
-alter table STEP modify id_adventure int unsigned;
-alter table STEP add constraint FK_ADVENTURE_STEP foreign key (id_adventure) references ADVENTURE(id_adventure);
-alter table STEP modify date_creation timestamp default localtimestamp();
+alter table ADVENTURE 
+	modify id_adventure int unsigned auto_increment primary key,
+	modify name varchar(40) not null unique,
+	modify description varchar(100) not null,
+	modify date_creation timestamp default localtimestamp();
 
-alter table ANSWER modify id_answer int unsigned auto_increment primary key;
-alter table ANSWER modify description varchar(100) not null;
-alter table ANSWER modify id_step_resolution int unsigned;
-alter table ANSWER add constraint FK_ANSWER_STEP foreign key (id_step_resolution) references STEP(id_step);
-alter table ANSWER modify date_creation timestamp default localtimestamp();
+alter table STEP 
+	modify id_step int unsigned auto_increment primary key,
+	modify description varchar(100) not null,
+	modify id_adventure int unsigned,
+	add constraint FK_ADVENTURE_STEP foreign key (id_adventure) references ADVENTURE(id_adventure),
+	modify date_creation timestamp default localtimestamp();
 
-alter table GAME modify id_game int unsigned auto_increment primary key;
-alter table GAME modify id_adventure int unsigned not null;
-alter table GAME modify username int unsigned not null;
-alter table GAME modify id_character int unsigned not null;
-alter table GAME modify id_step int unsigned not null;
-alter table GAME add constraint FK_GAME_ADVENTURE foreign key (id_adventure) references ADVENTURE(id_adventure);
-alter table GAME add constraint FK_GAME_USER foreign key (username) references USER(id_user);
-alter table GAME add constraint FK_GAME_CHARACTER foreign key (id_character) references PROYECTO_CHARACTER(id_character);
-alter table GAME add constraint FK_GAME_STEP foreign key (id_step) references STEP(id_step);
-alter table GAME modify date_creation timestamp default localtimestamp();
+alter table ANSWER 
+	modify id_answer int unsigned auto_increment primary key,
+	modify description varchar(100) not null,
+	modify id_step_resolution int unsigned,
+	add constraint FK_ANSWER_STEP foreign key (id_step_resolution) references STEP(id_step),
+	modify date_creation timestamp default localtimestamp();
 
-alter table HAS modify id_adventure int unsigned not null;
-alter table HAS modify id_character int unsigned not null;
-alter table HAS add constraint FK_HAS_ADVENTURE foreign key (id_adventure) references ADVENTURE(id_adventure);
-alter table HAS add constraint FK_HAS_CHARACTER foreign key (id_character) references PROYECTO_CHARACTER(id_character);
-alter table HAS modify date_creation timestamp default localtimestamp();
+alter table GAME 
+	modify id_game int unsigned auto_increment primary key,
+	modify id_adventure int unsigned not null,
+	modify username int unsigned not null,
+	modify id_character int unsigned not null,
+	modify id_step int unsigned not null,
+	add constraint FK_GAME_ADVENTURE foreign key (id_adventure) references ADVENTURE(id_adventure),
+	add constraint FK_GAME_USER foreign key (username) references USER(id_user),
+	add constraint FK_GAME_CHARACTER foreign key (id_character) references PROYECTO_CHARACTER(id_character),
+	add constraint FK_GAME_STEP foreign key (id_step) references STEP(id_step),
+	modify date_creation timestamp default localtimestamp();
+
+alter table HAS 
+	modify id_adventure int unsigned not null,
+	modify id_character int unsigned not null,
+	add constraint FK_HAS_ADVENTURE foreign key (id_adventure) references ADVENTURE(id_adventure),
+	add constraint FK_HAS_CHARACTER foreign key (id_character) references PROYECTO_CHARACTER(id_character),
+	modify date_creation timestamp default localtimestamp();
