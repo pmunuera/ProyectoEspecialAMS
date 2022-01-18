c0d42b56d8ed0ee84c3fdb138a11a176fa5de135
munue <munuera03@gmail.com>
2022-01-18 21:37:52 +0100

    Update 3 programmes

--- M3/Diccionarios.py
+++ M3/Diccionarios.py
@@ -7,9 +7,21 @@ adventures={id_adventure:{'Name': adventure_name,
                           'Description':adventure_description,
                           'Characters': adventure_characters}
             }
-for i in t:
+query='select * from ADVENTURE'
+
+cur.execute(query)
+
+tupla=cur.fetchall()
+
+for i in tupla:
     adventures[i[0]]={}#1:{}
     adventures[i[0]]['Name']=i[1]
+    adventures[i[0]]['Description'] = i[2]
+    query2=f"select name from CHARACTER where id_character in (select id_character from HAS where id_adventure='{i[0]}')"
+    cur.execute(query2)
+    tupla2=cur.fetchall()
+    for j in tupla2:
+        adventures[i[0]]['Characters'] = j[1]
 
 characters={
 
@@ -28,11 +40,41 @@ print(characters)
 Answers_ByStep_Adventure={{idAnswers_ByStep_Adventure, idByStep_Adventure}:{
                             'Description':description,
                             'Resolution_Answer':Resolution_Answer},
-                            'NextStep_Adventure':NextStep}
-BySteps_Adventure={id_ByStep_Adventure:{'Description':Description,
+                            'NextStep_Adventure':NextStep
+}
+query='select * from ANSWER'
+
+cur.execute(query)
+
+tupla=cur.fetchall()
+
+for i in tupla:
+    Answers_ByStep_Adventure[{i[0],i[2]}]={}#1:{}
+    Answers_ByStep_Adventure[{i[0],i[2]}]['Description']=i[1]
+
+ByStep_Adventure={id_ByStep_Adventure:{'Description':Description,
                                        'answers_in_step':answers_in_step,
                                        "Final_Step":Final_Step}
                   }
+query='select * from STEP'
+
+cur.execute(query)
+
+tupla=cur.fetchall()
+
+for i in tupla:
+    ByStep_Adventure[i[0]]={}#1:{}
+    ByStep_Adventure[i[0]]['Description']=i[1]
+    query2=f"select * from ANSWER where id_step_resolution in '{i[0]}'"
+    cur.execute(query2)
+
+    tupla2 = cur.fetchall()
+    ByStep_Adventure[i[0]]['answers_in_step']=[]
+    for j in tupla2:
+        ByStep_Adventure[i[0]]['answers_in_step'].append(j[0])
+    ByStep_Adventure[i[0]]['Final_Step'] = i[2]
+
+
 replayAdventures={idGame:{'idUser':id_user,'Username':username,
                           'IdAdventure':id_adventure,
                           'Name':adventure_name,
